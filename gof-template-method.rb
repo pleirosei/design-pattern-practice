53ad3bebfa3b5e0efad0897e47d7a2009931f64a
Sean Livingston <sean.d.livingston@gmail.com>
2015-01-14 15:47:20 -0500

    Template Method: Separating things that change from things that remain the same

--- gof-template-method.rb
+++ gof-template-method.rb
@@ -1,33 +1,102 @@
 class Report
 	def initialize
 		@title = 'Monthly Report'
-		@text = ['Thing are going', 'really, really well']
+		@text  = ['Things are going', 'really, really well']
 	end
 
-	def output_report(format)
-		if format == :plain
-			puts "*** #{@title} ***"
-		elsif format == :html
-			puts '<html>'
-			puts ' <head>'
-			puts "  <title>#{@title}</title>"
-			puts ' </head>'
-			puts ' <body>'
-		else
-			raise "Unknown format: #{format}"
-		end
+	def output_report
+		output_start
+		output_head
+		output_body_start
+		output_body
+		output_body_end
+		output_end
+	end
 
+	def output_body
 		@text.each do |line|
-			if format == :plain
-				puts(line)
-			else
-				puts "<p>#{line}</p>"
-			end
+			output_line(line)
 		end
+	end
 
-		if format == :html
-			puts ' </body>'
-			puts '</html>'
-		end
+	def output_start
+		raise 'Called abstract method: output_start'
+	end
+
+	def output_head
+		raise 'Called abstract method: output_head'
+	end
+
+	def output_body_start
+		raise 'Called abstract method: output_body_start'
+	end
+
+	def output_line(line)
+		raise 'Called abstract method: output_line'
+	end
+
+	def output_body_end
+		raise 'Called abstract method: output_body_end'
+	end
+
+	def output_end
+		raise 'Called abstract method: output_end'
 	end
 end
+
+class HTMLReport < Report
+	def output_start
+		puts '<html>'
+	end
+
+	def output_head
+		puts ' <head>'
+		puts "  <title>#{@title}</title"
+		puts ' </head>'
+	end
+
+	def output_body_start
+		puts '<body>'
+	end
+
+	def output_line(line)
+		puts " <p>#{line}</p>"
+	end
+
+	def output_body_end
+		puts '</body>'
+	end
+
+	def output_end
+		puts '</html>'
+	end
+end
+
+class PlainTextReport < Report
+	def output_start
+	end
+
+	def output_head
+		puts "**** #{@title} ****"
+		puts
+	end
+
+	def output_body_start
+	end
+
+	def output_line(line)
+		puts line
+	end
+
+	def output_body_end
+	end
+
+	def output_end
+	end
+end
+
+report = HTMLReport.new
+report.output_report
+
+report1 = PlainTextReport.new
+report1.output_report
